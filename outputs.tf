c4cdf4eb80fc5c44449ca757108bb3c8de7fb59e
Anisimova Karina <51861302+Karina5005@users.noreply.github.com>
2024-04-18 11:19:26 +0300

    fix: Apply terraform fmt (#7)

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
 output "name" {
   description = "The name of a created template"
-  value = huaweicloud_lts_notification_template.template.name
+  value       = huaweicloud_lts_notification_template.template.name
 }
